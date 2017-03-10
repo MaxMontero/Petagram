9018c45a3eef5f8fcbfc6bbcfc78d34e86fe0b34
Max Montero <montero.n.max@gmail.com>
2017-03-10 10:46:35 -0500

    Comit Inicial Agregado Raised Button

---
+++ app/src/main/java/maxmontero/com/petagram/DetalleMascotaActivity.java
@@ -0,0 +1,13 @@
+package maxmontero.com.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetalleMascotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_mascota);
+    }
+}
---
+++ app/src/main/java/maxmontero/com/petagram/ListadoMascotasActivity.java
@@ -0,0 +1,13 @@
+package maxmontero.com.petagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListadoMascotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listado_mascotas);
+    }
+}
