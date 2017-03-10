fd08d6997e1ffb52fd2ff0e3e19e5cde0c0d59fb
Max Montero <montero.n.max@gmail.com>
2017-03-10 11:39:37 -0500

    Agregado Floating Action Button a la aplicación

--- app/src/main/java/maxmontero/com/petagram/MainActivity.java
+++ app/src/main/java/maxmontero/com/petagram/MainActivity.java
@@ -1,7 +1,9 @@
 package maxmontero.com.petagram;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        agregarFAB();
+    }
+
+    //metodo para agregar el floating action button
+    public void agregarFAB(){
+        FloatingActionButton fabMain = (FloatingActionButton) findViewById(R.id.fab_main);
+        fabMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
